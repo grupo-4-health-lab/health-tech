52c8c5c89cc1152f717b93d43780daa0c5cf0cd4
Edu Caribe <edu.caribe@hotmail.com>
2021-05-08 15:06:52 -0300

    refactor: improve css spacing and modulize components

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -1,4 +1,4 @@
-// Modules
+// Ng Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,29 +8,25 @@ import { AppComponent } from './app.component';
 import { UserService } from './services/user.service';
 import { AuthService } from './services/auth.service';
 
-// Pages
+// Page Components
 import { LoginComponent } from './pages/login/login.page';
-import { PasswordResetComponent } from './pages/password-reset/password-reset.page';
 import { NotFoundComponent } from './pages/not-found/not-found.page';
-import { SystemComponent } from './pages/system/system.page';
 
-// Child-components
-import { SendEmailComponent } from './pages/password-reset/send-email/send-email.component';
-import { ApplyCodeComponent } from './pages/password-reset/apply-code/apply-code.component';
+// Page Modules
+import { SystemModule } from './pages/system/system.module';
+import { PasswordResetModule } from './pages/password-reset/password-reset.module';
 
 @NgModule({
     declarations: [
         AppComponent,
         LoginComponent,
-        PasswordResetComponent,
-        NotFoundComponent,
-        SendEmailComponent,
-        ApplyCodeComponent,
-        SystemComponent
+        NotFoundComponent
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        SystemModule,
+        PasswordResetModule
     ],
     providers: [
         UserService,
---
+++ Front-End/src/app/pages/system/system.module.ts
@@ -0,0 +1,15 @@
+// Module
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Components
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SystemModule { }
