538b8fd506f69b3ab1fbfa0c75426787f93f9454
Edu Caribe <edu.caribe@hotmail.com>
2021-05-13 00:00:39 -0300

    feat: add responsive style to side menu

--- Front-End/src/app/pages/system/main-components/side-menu/side-menu.component.ts
+++ Front-End/src/app/pages/system/main-components/side-menu/side-menu.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
-import { cilRoom, cilFork, cilGroup, cilExitToApp, cilHome } from '@coreui/icons';
+import { cilRoom, cilFork, cilGroup, cilExitToApp, cilHome, cilHamburgerMenu } from '@coreui/icons';
 import { IconSetService } from '@coreui/icons-angular';
 
 @Component({
-  selector: 'system-side-menu',
-  templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.scss'],
-  providers: [IconSetService]
+    selector: 'system-side-menu',
+    templateUrl: './side-menu.component.html',
+    styleUrls: ['./side-menu.component.scss'],
+    providers: [IconSetService]
 })
 export class SideMenuComponent {
-    constructor(public iconSet: IconSetService) {
-        iconSet.icons = { cilRoom, cilFork, cilGroup, cilExitToApp, cilHome };
+    /** Flag de menu aberto */
+    public opened: boolean = false;
+
+    constructor(
+        public iconSet: IconSetService
+    ) {
+        iconSet.icons = {
+            cilRoom,
+            cilFork,
+            cilGroup,
+            cilExitToApp,
+            cilHome,
+            cilHamburgerMenu
+        };
+    }
+
+    /**
+     * Altera o status do menu
+     *
+     * @public
+     *
+     * @return {void}
+     */
+    public toggleMenu(): void {
+        this.opened = !this.opened;
     }
 }
